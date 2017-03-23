88f5c30295009f85b3107a3092e7b6b6709fc621
yangtuocn <yangtuocn@gmail.com>
2017-03-23 20:45:25 +0000

    1

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	port = int(os.enviorn.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host = '0.0.0.0', port = port)
